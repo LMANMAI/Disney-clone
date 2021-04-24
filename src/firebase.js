564a2d2d9508cfeae2d013496c96cd7554a91489
Lucas Maidana <lucas_sknr.010@live.com>
2021-04-24 05:08:41 -0300

    initialize firebase, now rest the complete auth function with redux

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from "firebase";
+const firebaseConfig = {
+  apiKey: "AIzaSyAwD2LMJtWgt-Y2-C7UKEZ5hrPmNHa8bbg",
+  authDomain: "disney-clone-3aec6.firebaseapp.com",
+  projectId: "disney-clone-3aec6",
+  storageBucket: "disney-clone-3aec6.appspot.com",
+  messagingSenderId: "761551625553",
+  appId: "1:761551625553:web:9844cca3df6baac4c0c770",
+  measurementId: "G-3Y30EWPWZ3",
+};
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+const storage = firebase.storage();
+
+export { auth, provider, storage };
+export default db;
--- src/pages/components/Header.js
+++ src/pages/components/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { auth, provider } from "../../firebase";
 
 const Nav = styled.nav`
   position: fixed;
@@ -13,6 +14,7 @@ const Nav = styled.nav`
   justify-content: space-between;
   align-items: center;
   padding: 0 30px;
+  letter-spacing: 15px;
   z-index: 3;
 `;
 const LogoImage = styled.a`
@@ -24,16 +26,126 @@ const LogoImage = styled.a`
   display: inline-block;
   img {
     display: block;
-    width: 100;
+    width: 100%;
+  }
+`;
+
+const NavMenu = styled.div`
+  align-items: center;
+  display: flex;
+  flex-flow: row nowrap;
+  height: 100%;
+  justify-content: flex-end;
+  margin: 0px;
+  padding: 0px;
+  position: relative;
+  margin-right: auto;
+  margin-left: 1.5rem;
+  text-transform: uppercase;
+  @media (max-width: 768px) {
+    display: none;
+  }
+
+  a {
+    display: flex;
+    align-items: center;
+    padding: 0 12px;
+    //align-items: center;
+    img {
+      height: 20px;
+      width: 20px;
+      z-index: auto;
+    }
+    span {
+      color: rgb(249, 249, 249);
+      font-size: 13px;
+      letter-spacing: 1.42px;
+      line-height: 1.08;
+      padding: 2px 0;
+      white-space: nowrap;
+      position: relative;
+      &:before {
+        content: "";
+        background-color: rgb(249, 249, 249);
+        border-radius: 0 0 4px 4px;
+        bottom: -6px;
+        height: 2px;
+        left: 0px;
+        opacity: 0;
+        position: absolute;
+        right: 0px;
+        transform-origin: left center;
+        transform: scaleX(0);
+        transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+        visibility: hidden;
+        width: auto;
+      }
+    }
+    &:hover {
+      span:before {
+        transform: scaleX(1);
+        visibility: visible;
+        opacity: 1 !important;
+      }
+    }
+  }
+`;
+const ButtonLogin = styled.a`
+  background-color: rgba(0, 0, 0, 0.6);
+  letter-spacing: 1.5px;
+  text-transform: uppercase;
+  padding: 8px 16px;
+  border: 1px solid #f9f9f9;
+  border-radius: 4px;
+  transition: all 0.2s ease-in-out;
+  &:hover {
+    background-color: #f9f9f9;
+    color: #090b13;
+    cursor: pointer;
+    border-color: transparent;
   }
 `;
 const Header = () => {
+  const handleAuth = () => {
+    auth
+      .signInWithPopup(provider)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => alert(error.message));
+  };
   return (
     <Nav>
       <LogoImage>
         <img src="/images/logo.svg" alt="Disney +" />
       </LogoImage>
-      <button>Login</button>
+      <NavMenu>
+        <a>
+          <img src="images/home-icon.svg" alt="Home" />
+          <span>home</span>
+        </a>
+        <a>
+          <img src="images/search-icon.svg" alt="Home" />
+          <span>search</span>
+        </a>
+        <a>
+          <img src="images/watchlist-icon.svg" alt="Home" />
+          <span>watchlist</span>
+        </a>
+        <a>
+          <img src="images/original-icon.svg" alt="Home" />
+          <span>originals</span>
+        </a>
+        <a>
+          <img src="images/movie-icon.svg" alt="Home" />
+          <span>movies</span>
+        </a>
+        <a>
+          <img src="images/series-icon.svg" alt="Home" />
+          <span>series</span>
+        </a>
+      </NavMenu>
+      <ButtonLogin onClick={handleAuth}>LOGIN</ButtonLogin>
     </Nav>
   );
 };
